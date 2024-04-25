57e84c84557570808fce44b886c222a439c6f984
amor1523 <amor152394@gmail.com>
2024-04-25 17:54:38 +0900

    [Complex] Initial Data, Code add/delete
    초기데이터 수정 및 코드수정

--- TextRpg/Program.cs
+++ TextRpg/Program.cs
@@ -17,19 +17,22 @@
         static void InitialDataSsetting()
         {
             // 캐릭터 정보
-            player = new Character("Chad", "전사", 1, 10, 5, 100, 300000f);
+            player = new Character("Chad", "전사", 1, 10, 5, 100, 1500f);
 
             // 아이템 정보 세팅
-            items = new Item[8];
+            items = new Item[11];
 
             items[0] = new Item("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", 1, 0, 5, 0, 1000f);
             items[1] = new Item("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 1, 0, 5, 0, 1800f);
             items[2] = new Item("스파르타의 갑옷", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 1, 0, 15, 0, 3500f);
-            items[3] = new Item("개발자의 한이 서린 갑옷", "전설의 개발자가 사용했다는 최강의 갑옷입니다.", 1, 0, 50, 100, 10000f);
+            items[3] = new Item("개발자의 한이 서린 갑옷", "전설의 개발자가 사용했다는 최강의 갑옷입니다.", 1, 0, 50, 200, 50000f);
             items[4] = new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 0, 2, 0, 0, 300f);
             items[5] = new Item("청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", 0, 5, 0, 0, 1500f);
             items[6] = new Item("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 0, 7, 0, 0, 2700f);
-            items[7] = new Item("죽 창", "평등해보이지만 데미지는 평등해보이지 않습니다.", 0, 20, 0, 0, 10000f);
+            items[7] = new Item("죽 창", "평등해보이지만 데미지는 평등해보이지 않습니다.", 0, 20, 0, 0, 100000f);
+            //items[8] = new Item("구리열쇠", "이상한 기호가 적혀있다. ⅳ", 2, 0, 0, 50, 0f);
+            //items[9] = new Item("은열쇠", "이상한 기호가 적혀있다. ⅷ", 2, 0, 0, 100, 0f);
+            //items[10] = new Item("금열쇠", "이상한 기호가 적혀있다. ⅵ", 2, 0, 0, 200, 0f);
 
             //상점 정보 세팅
             shop = new List<Item>();
@@ -208,17 +211,18 @@
             Console.Write("원하시는 행동을 입력해주세요.\n>> ");
 
             int input = CheckValidInput(0, inventory.Count);
-                switch (input)
-                {
-                    case 0:
-                        DisplayInventory();
-                        break;
-                    default:
-                        if (inventory[input - 1].Equipped == 0) inventory[input - 1].Equipped += 1;
-                        else inventory[input - 1].Equipped -= 1;
-                        Displayitems();
-                        break;
-                }
+            switch (input)
+            {
+                case 0:
+                    DisplayInventory();
+                    break;
+                default:
+                    if (inventory[input - 1].Equipped == 0) inventory[input - 1].Equipped += 1;
+                    else inventory[input - 1].Equipped -= 1;
+                    Displayitems();
+                    break;
+                        
+            }
             
         }
 
@@ -451,6 +455,9 @@
         public int Def { get; }
         public int Hp { get; }
         public float Gold { get; set; }
+        public string? WeaponEquipe { get; set; }
+        public string? ArmorEquipe { get; set; }
+        public string? KeyEquipe { get; set; }
 
         public Character(string name, string job, int level, int atk, int def, int hp, float gold)
         {
@@ -475,6 +482,7 @@
         public float Price { get; }
         public int Purchased { get; set; }
         public int Equipped { get; set; }
+        
 
         public Item(string name, string description, int category, int atk, int def, int hp, float price)
         {
